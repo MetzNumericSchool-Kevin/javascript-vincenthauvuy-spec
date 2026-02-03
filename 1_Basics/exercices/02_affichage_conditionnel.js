34d5f3f6e2997bf6bab081eb463141c5eb0104ec
vincenthauvuy-spec <vincent.hauvuy@stagiairesmns.fr>
2026-02-03 10:40:28 +0100

    exo2

--- 1_Basics/exercices/02_affichage_conditionnel.js
+++ 1_Basics/exercices/02_affichage_conditionnel.js
@@ -1,5 +1,11 @@
 // Affichage conditionnel
 
 // Variables de l'exercice précédent (modifie selon tes valeurs)
-const nom_boutique = "La Potion Enchantée";
-const boutique_ouverte = true;
+// Affiche dans ta `console` => `Bienvenue dans la boutique <nom_boutique> Aventurier ! 🎉` si la boutique est ouverte, sinon affiche `La boutique <nom_boutique> est fermée, revenez plus tard Aventurier ! 😴`
+const maBoutique = "La Potion Magique";
+const boutiqueOuverte = true;
+if (boutiqueOuverte) {
+    console.log("Bienvenue dans la boutique " + maBoutique + " Aventurier ! 🎉");
+} else {
+    console.log("La boutique " + maBoutique + " est fermée, revenez plus tard Aventurier ! 😴");
+}
