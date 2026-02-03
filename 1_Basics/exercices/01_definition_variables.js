0b55098c53f3096d796ea593c0ffe3fe0033fd68
vincenthauvuy-spec <vincent.hauvuy@stagiairesmns.fr>
2026-02-03 10:28:22 +0100

    exo1

--- 1_Basics/exercices/01_definition_variables.js
+++ 1_Basics/exercices/01_definition_variables.js
@@ -1,1 +1,11 @@
 // Définition de variables
+// Déclare les variables pour ta boutique de potions :
+
+// - Déclare une constante pour le nom de ta boutique.
+// - Déclare une variable pour le nombre de potions de soins en stock, la valeur par défaut de ton choix.
+// - Déclare une variable pour indiquer le prix d'une potion de soin, la valeur par défaut de ton choix.
+// - Déclare une variable pour indiquer si la boutique est ouverte ou non.
+const maBoutique = "La Potion Magique";
+let potionsDeSoinsEnStock = 50;
+let prixPotionDeSoin = 25; // en pièces d'or
+let boutiqueOuverte = true;
